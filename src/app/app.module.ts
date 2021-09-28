3803fd416379db323f229711fc75563b87d27ba8
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-28 23:11:56 +0300

    add error 404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { CourseComponent } from './course/course.component';
 import { JoinnowComponent } from './joinnow/joinnow.component';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AboutComponent } from './about/about.component';
     HomeComponent,
     CourseComponent,
     JoinnowComponent,
-    AboutComponent
+    AboutComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
