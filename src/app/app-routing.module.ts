17e10e29eeb2af2f9a83848d8fe48c4d55523874
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-28 11:29:58 +0300

    import components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CourseComponent } from './course/course.component';
+import { JoinnowComponent } from './joinnow/joinnow.component';
+import { AboutComponent } from './about/about.component'
 
 const routes: Routes = [
   {path:'',component:HomeComponent},
