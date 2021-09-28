6cee56cc40251e1a88e5aecafa4ad0537e08a65b
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-28 08:53:25 +0300

    add routerlinks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'course',component:CourseComponent},
+  {path:'joinnow',component:JoinnowComponent},
+  {path:'about',component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
